5a8b9270915c4e8a073468067de83f988c6fb33b
KimJungChul <kjc5443@gmail.com>
2019-01-29 02:22:04 +0900

    adding get-job-status lambda_function.py

---
+++ aws/cpu-memory/feature-generation/get-job-status/lambda_function.py
@@ -0,0 +1,18 @@
+import boto3
+
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+    num_of_file = int(event['num_of_file'])
+    all_keys = []
+    
+    bucket = "kmu-serverless-feature-extract"
+    
+    for obj in s3.Bucket(bucket).objects.all():
+        all_keys.append(obj.key)
+    print "Number of File : " + str(len(all_keys))
+    
+    if num_of_file == len(all_keys):
+        return {"status" : "SUCCEEDED"}
+    else:
+        return {"status" : "FAILED"}

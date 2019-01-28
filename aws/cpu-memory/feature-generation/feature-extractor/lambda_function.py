76eb77ecd38219989188d9b8906c82968c1fe864
KimJungChul <kjc5443@gmail.com>
2019-01-29 02:21:30 +0900

    adding feature-extractor lambda_function.py

---
+++ aws/cpu-memory/feature-generation/feature-extractor/lambda_function.py
@@ -0,0 +1,36 @@
+import boto3
+import pandas as pd
+import numpy as np
+from time import time
+import re
+
+s3 = boto3.client('s3')
+
+cleanup_re = re.compile('[^a-z]+')
+def cleanup(sentence):
+    sentence = sentence.lower()
+    sentence = cleanup_re.sub(' ', sentence).strip()
+    return sentence
+
+def lambda_handler(event, context):
+    bucket = event['input_bucket']
+    key = event['key']
+    path = bucket + "/" + key
+    df = pd.read_csv('s3://'+path)
+
+    start = time()
+    df['Text'] = df['Text'].apply(cleanup)
+    text = df['Text'].tolist()
+    result = set()
+    for item in text:
+        result.update(item.split())
+    print("Number of Feature : " + str(len(result)))
+
+    feature = str(list(result))
+    feature = feature.lstrip('[').rstrip(']').replace(' ', '')
+    latency = time() - start
+    print latency
+
+    write_key = event['key'].split('.')[0] + ".txt"
+    s3.put_object(Body=feature, Bucket='kmu-serverless-feature-extract', Key=write_key)
+    return latency

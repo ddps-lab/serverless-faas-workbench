a32bff2383d17c9475b8a8b6c7fe18b4cb37ec75
KimJungChul <kjc5443@gmail.com>
2019-01-28 14:28:36 +0900

    adding float-operations lambda_function.py

---
+++ aws/cpu-memory/float-operation/lambda_function.py
@@ -0,0 +1,16 @@
+import math
+from time import time
+
+def float_operations(N):
+	start = time()
+	for i in range(0, N):
+		sin_i = math.sin(i)
+		cos_i = math.cos(i)
+		sqrt_i = math.sqrt(i)
+	latency = time() - start
+	return latency
+
+def lambda_handler(event, context):
+    N = int(event['N'])
+    result = float_operations(N)
+    print(result)

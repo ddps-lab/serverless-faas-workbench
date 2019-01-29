66e9eaaccb2906bed8a28cf0ad9901027fd2705f
KimJungChul <kjc5443@gmail.com>
2019-01-29 19:02:31 +0900

    adding dd main.py

---
+++ google/disk/dd/main.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+def function_handler(request):
+    request_json = request.get_json(silent=True)
+    bs = 'bs='+request_json['bs']
+    count = 'count='+request_json['count']
+    print(bs)
+    print(count)
+    out_fd = open('/tmp/io_write_logs','w')
+    a = subprocess.Popen(['dd', 'if=/dev/zero', 'of=/tmp/out', bs, count], stderr=out_fd)
+    a.communicate()
+    
+    output = subprocess.check_output(['ls', '-alh', '/tmp/'])
+    print(output)
+
+    
+    output = subprocess.check_output(['du', '-sh', '/tmp/'])
+    print(output)
+                               
+    with open('/tmp/io_write_logs') as logs:
+        result = str(logs.readlines()[2]).replace('\n', '')
+        print(result)
+        return result

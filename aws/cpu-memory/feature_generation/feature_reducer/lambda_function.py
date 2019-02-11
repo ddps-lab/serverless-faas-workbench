5434151e8f24ccdf0a2884f10160b16809625b38
KimJungChul <kjc5443@gmail.com>
2019-02-11 13:25:07 +0900

    feature_reducer refactoring code

--- aws/cpu-memory/feature_generation/feature_reducer/lambda_function.py
+++ aws/cpu-memory/feature_generation/feature_reducer/lambda_function.py
@@ -1,24 +1,28 @@
 import boto3
 from time import time
-import re
 
 s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
+
 def lambda_handler(event, context):
-    bucket = s3.Bucket('kmu-serverless-feature-extract')
+    bucket = event['input_bucket']
+    s3_bucket = s3.Bucket(bucket)
+
     result = set()
     latency = 0
-    for obj in bucket.objects.all():
-        key = obj.key
+
+    for obj in s3_bucket.objects.all():
         body = obj.get()['Body'].read()
         start = time()
         feature = body.replace("'", '').split(',')
         result.update(feature)
         latency += time() - start
-    print len(result)
-    print latency
+
+    print(len(result))
+    print(latency)
     
     write_key = "result.txt"
-    s3_client.put_object(Body=str(result), Bucket='kmu-serverless-feature-extract', Key=write_key)
+    s3_client.put_object(Body=str(result), Bucket=bucket, Key=write_key)
+
     return latency

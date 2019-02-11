ec7153139633fbfd8d016415661cbb3d03f77e5d
KimJungChul <kjc5443@gmail.com>
2019-02-11 13:35:39 +0900

    float_operation refactoring code

--- aws/cpu-memory/float_operation/lambda_function.py
+++ aws/cpu-memory/float_operation/lambda_function.py
@@ -1,17 +1,19 @@
 import math
 from time import time
 
-def float_operations(N):
+
+def float_operations(n):
     start = time()
-    for i in range(0, N):
+    for i in range(0, n):
         sin_i = math.sin(i)
         cos_i = math.cos(i)
         sqrt_i = math.sqrt(i)
     latency = time() - start
     return latency
 
+
 def lambda_handler(event, context):
-    N = int(event['N'])
-    result = float_operations(N)
+    n = int(event['n'])
+    result = float_operations(n)
     print(result)
     return result

fad2acaf2c38fea529b8866f5c253505c8991114
KimJungChul <kjc5443@gmail.com>
2019-04-24 15:11:00 +0900

    refactoring linpack code

--- azure/cpu-memory/linpack/__init__.py
+++ azure/cpu-memory/linpack/__init__.py
@@ -1,25 +1,25 @@
 import azure.functions as func
-from numpy import matrix, array, linalg, random, amax, asscalar
+from numpy import matrix, linalg, random, amax
 from time import time
-import logging
+
+
 
 def linpack(N):
-    eps=2.22e-16
+    eps = 2.22e-16
 
-    ops=(2.0*N)*N*N/3.0+(2.0*N)*N
+    ops = (2.0*N)*N*N/3.0+(2.0*N)*N
 
     # Create AxA array of random numbers -0.5 to 0.5
-    A=random.random_sample((N,N))-0.5
-    B=A.sum(axis=1)
+    A = random.random_sample((N, N))-0.5
+    B = A.sum(axis=1)
 
     # Convert to matrices
-    A=matrix(A)
+    A = matrix(A)
 
-    B=matrix(B.reshape((N,1)))
-    na=amax(abs(A.A))
+    B = matrix(B.reshape((N, 1)))
 
     start = time()
-    X=linalg.solve(A,B)
+    X = linalg.solve(A, B)
     latency = time() - start
 
     mflops = (ops*1e-6/latency)
@@ -33,9 +33,7 @@ def linpack(N):
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    N = req.params.get('N')
-    N = int(N)
-    latency = linpack(N)
-    logging.info(latency)
-    return func.HttpResponse(str(latency))
+    N = int(req.params.get('N'))
+    latency = str(linpack(N))
+
+    return func.HttpResponse(latency)

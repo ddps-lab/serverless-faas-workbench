d1e2fc065b6f5c7e241ccbb9300c5a49f1040a53
KimJeongChul <kjc5443@gmail.com>
2019-04-25 01:42:09 +0900

    refactoring model_training code

--- aws/cpu-memory/model_training/lambda_function.py
+++ aws/cpu-memory/model_training/lambda_function.py
@@ -1,13 +1,10 @@
 import boto3
-import botocore
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.externals import joblib
 
 import pandas as pd
-import numpy as np
-import uuid
 from time import time
 import re
 
@@ -15,15 +12,22 @@ s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
 cleanup_re = re.compile('[^a-z]+')
+
+
 def cleanup(sentence):
     sentence = sentence.lower()
     sentence = cleanup_re.sub(' ', sentence).strip()
+
     return sentence
 
+
 def lambda_handler(event, context):
-    #buckt = event['input_bucket']
-    #key = event['key']
-    df = pd.read_csv('s3://kmu-serverless-dataset/Reviews.csv')
+    input_bucket = event['input_bucket']
+    object_key = event['object_key']
+    model_bucket = event['model_bucket']
+    model_object_key = event['model_object_key']
+
+    df = pd.read_csv('s3://'+input_bucket+object_key)
 
     start = time()
     df['train'] = df['Text'].apply(cleanup)
@@ -40,8 +44,6 @@ def lambda_handler(event, context):
     model_file_path = "/tmp/lr_model.pk"
     joblib.dump(model, model_file_path)
 
-    model_bucket = 'kmu-serverless-lambda-model'
-    model_object_key = 'lr_model.pk'
-
     s3.Object(model_bucket, model_object_key).load()
+
     return latency

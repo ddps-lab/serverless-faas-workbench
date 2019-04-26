f81aac4140c0107ed9be29f67a53e8951b445a4a
KimJungChul <kjc5443@gmail.com>
2019-04-26 23:35:00 +0900

    refactoring ml_lr_prediction code

--- aws/cpu-memory/model_serving/ml_lr_prediction/lambda_function.py
+++ aws/cpu-memory/model_serving/ml_lr_prediction/lambda_function.py
@@ -1,43 +1,55 @@
 import boto3
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
 from sklearn.externals import joblib
 
 import pandas as pd
-import numpy as np
 
 from time import time
 import os
 import re
 
 s3_client = boto3.client('s3')
-
-if not os.path.isfile('/tmp/lr_model.pk'):
-    s3_client.download_file("kmu-serverless-lambda-model", 'lr_model.pk', '/tmp/lr_model.pk')
-
+tmp = '/tmp/'
 cleanup_re = re.compile('[^a-z]+')
+
+
 def cleanup(sentence):
     sentence = sentence.lower()
     sentence = cleanup_re.sub(' ', sentence).strip()
     return sentence
 
+
 def lambda_handler(event, context):
-    x = event['input']
-    df = pd.read_csv('s3://kmu-serverless-lambda-dataset/reviews0.csv')
+    x = event['x']
+
+    dataset_object_key = event['dataset_object_key']
+    dataset_bucket = event['dataset_bucket']
+
+    model_object_key = event['model_object_key'] # example : lr_model.pk
+    model_bucket = event['model_bucket']
+
+    model_path = tmp + model_object_key
+    if not os.path.isfile(model_path):
+        s3_client.download_file(model_bucket, model_object_key, model_path)
+
+    dataset_path = 's3://'+dataset_bucket+'/'+dataset_object_key
+    dataset = pd.read_csv(dataset_path)
 
     start = time()
+
     df_input = pd.DataFrame()
-    df_input['x']  = [x]
+    df_input['x'] = [x]
     df_input['x'] = df_input['x'].apply(cleanup)
 
-    df['train'] = df['Text'].apply(cleanup)
-    tfidf_vect = TfidfVectorizer(min_df=100).fit(df['train'])
-    print(len(tfidf_vect.get_feature_names()))
+    dataset['train'] = dataset['Text'].apply(cleanup)
+
+    tfidf_vect = TfidfVectorizer(min_df=100).fit(dataset['train'])
+
     X = tfidf_vect.transform(df_input['x'])
 
-    model = joblib.load('/tmp/lr_model.pk')
+    model = joblib.load(model_path)
     y = model.predict(X)
-    print(y)
+
     latency = time() - start
-    print(latency)
-    return latency
+
+    return {'y': y, 'latency': latency}

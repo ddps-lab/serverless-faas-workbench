c3dd5d0d55961098cd3168a13118e7bd76e82504
KimJungChul <kjc5443@gmail.com>
2019-04-29 15:04:15 +0900

    refactoring rnn_generate_character_level code

--- aws/cpu-memory/model_serving/rnn_generate_character_level/lambda_function.py
+++ aws/cpu-memory/model_serving/rnn_generate_character_level/lambda_function.py
@@ -3,72 +3,14 @@ import os
 import pickle
 import numpy as np
 import torch
-import torch.nn as nn
-from torch.autograd import Variable
-from time import time
-
 import rnn
 
-BUCKET_NAME = "kmu-serverless-lambda-model"
+from time import time
+
+
 s3_client = boto3.client('s3')
 
-# Load preprocessing parameters
-if not os.path.isfile('/tmp/rnn_params.pkl'):
-    s3_client.download_file(BUCKET_NAME, 'rnn_params.pkl', '/tmp/rnn_params.pkl')
-with open('/tmp/rnn_params.pkl', 'rb') as pkl:
-    params = pickle.load(pkl)
-all_categories = params['all_categories']
-n_categories = params['n_categories']
-all_letters = params['all_letters']
-n_letters = params['n_letters']
-
-# Check if models are available
-# Download model from S3 if model is not already present
-if not os.path.isfile('/tmp/rnn_model.pth'):
-    s3_client.download_file(BUCKET_NAME, 'rnn_model.pth', '/tmp/rnn_model.pth')
-rnn_model = rnn.RNN(n_letters, 128, n_letters, n_categories=n_categories)
-rnn_model.load_state_dict(torch.load("/tmp/rnn_model.pth"))
-rnn_model.eval()
-
-def inputTensor(line):
-    tensor = torch.zeros(len(line), 1, n_letters)
-    for li in range(len(line)):
-        letter = line[li]
-        tensor[li][0][all_letters.find(letter)] = 1
-    return tensor
-
-def categoryTensor(category):
-    li = all_categories.index(category)
-    tensor = torch.zeros(1, n_categories)
-    tensor[0][li] = 1
-    return tensor
-
-# Sample from a category and starting letter
-def sample(category, start_letter='A'):
-    category_tensor = Variable(categoryTensor(category))
-    input = Variable(inputTensor(start_letter))
-    hidden = rnn_model.initHidden()
-
-    output_name = start_letter
-
-    max_length=20
-    for i in range(max_length):
-        output, hidden = rnn_model(category_tensor, input[0], hidden)
-        topv, topi = output.data.topk(1)
-        topi = topi[0][0]
-        if topi == n_letters - 1:
-            break
-        else:
-            letter = all_letters[topi]
-            output_name += letter
-        input = Variable(inputTensor(letter))
-
-    return output_name
-
-# Get multiple samples from one category and multiple starting letters
-def samples(category, start_letters='ABC'):
-    for start_letter in start_letters:
-        yield sample(category, start_letter)
+tmp = "/tmp/"
 
 """
 Language
@@ -76,11 +18,42 @@ Language
  - English, Spanish, Arabic, Crech, Russian, Irish, Dutch
  - Scottish, Vietnamese, Korean, Japanese
 """
+
+
 def lambda_handler(event, context):
     language = event['language']
     start_letters = event['start_letters']
+
+    model_parameter_object_key = event['model_parameter_object_key']  # example : rnn_params.pkl
+    model_object_key = event['model_object_key']  # example : rnn_model.pth
+    model_bucket = event['model_bucket']
+
+    # Load preprocessing parameters
+    # Check if model parameters are available
+    parameter_path = tmp + model_parameter_object_key
+    if not os.path.isfile(parameter_path):
+        s3_client.download_file(model_bucket, model_parameter_object_key, parameter_path)
+
+    with open(parameter_path, 'rb') as pkl:
+        params = pickle.load(pkl)
+
+    all_categories = params['all_categories']
+    n_categories = params['n_categories']
+    all_letters = params['all_letters']
+    n_letters = params['n_letters']
+
+    # Check if models are available
+    # Download model from S3 if model is not already present
+    model_path = tmp + model_object_key
+    if not os.path.isfile(model_path):
+        s3_client.download_file(model_bucket, model_object_key, model_path)
+
+    rnn_model = rnn.RNN(n_letters, 128, n_letters, all_categories, n_categories, all_letters, n_letters)
+    rnn_model.load_state_dict(torch.load(model_path))
+    rnn_model.eval()
+
     start = time()
-    output_names = list(samples(language, start_letters))
+    output_names = list(rnn_model.samples(language, start_letters))
     latency = time() - start
     print(output_names)
     print(latency)

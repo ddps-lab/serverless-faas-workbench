8a78f86b32f7d38632fab7140180821196b6b1f6
KimJungChul <kjc5443@gmail.com>
2019-09-20 14:42:22 +0900

    adding chameleon

---
+++ aws/cpu-memory/chameleon/lambda_function.py
@@ -0,0 +1,39 @@
+import functools
+from time import time
+import six
+import json
+from chameleon import PageTemplate
+
+
+BIGTABLE_ZPT = """\
+<table xmlns="http://www.w3.org/1999/xhtml"
+xmlns:tal="http://xml.zope.org/namespaces/tal">
+<tr tal:repeat="row python: options['table']">
+<td tal:repeat="c python: row.values()">
+<span tal:define="d python: c + 1"
+tal:attributes="class python: 'column-' + %s(d)"
+tal:content="python: d" />
+</td>
+</tr>
+</table>""" % six.text_type.__name__
+
+
+def lambda_handler(event, context):
+    num_of_rows = event['num_of_rows']
+    num_of_cols = event['num_of_cols']
+
+    start = time()
+    tmpl = PageTemplate(BIGTABLE_ZPT)
+
+    data = {}
+    for i in range(num_of_cols):
+        data[str(i)] = i
+
+    table = [data for x in range(num_of_rows)]
+    options = {'table': table}
+
+    data = tmpl.render(options=options)
+    latency = time() - start
+
+    result = json.dumps({'latency': latency, 'data': data})
+    return result

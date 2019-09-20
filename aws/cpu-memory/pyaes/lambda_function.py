45df23206169e6db1eefde724e48cc3279df0f10
KimJungChul <kjc5443@gmail.com>
2019-09-20 14:36:56 +0900

    adding pyaes

---
+++ aws/cpu-memory/pyaes/lambda_function.py
@@ -0,0 +1,33 @@
+from time import time
+import random
+import string
+import pyaes
+
+
+def generate(length):
+    letters = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def lambda_handler(event, context):
+    length_of_message = event['lenght_of_message']
+    num_of_iterations = event['num_of_iterations']
+
+    message = generate(length_of_message)
+
+    # 128-bit key (16 bytes)
+    KEY = b'\xa1\xf6%\x8c\x87}_\xcd\x89dHE8\xbf\xc9,'
+
+    start = time()
+    for loops in range(num_of_iterations):
+        aes = pyaes.AESModeOfOperationCTR(KEY)
+        ciphertext = aes.encrypt(message)
+        print(ciphertext)
+
+        aes = pyaes.AESModeOfOperationCTR(KEY)
+        plaintext = aes.decrypt(ciphertext)
+        print(plaintext)
+        aes = None
+
+    latency = time() - start
+    return {'ciphertext':ciphertext, 'latency':latency}

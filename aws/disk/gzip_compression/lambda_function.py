08e76cadca6aba4832b07e7cbbe213b4e1024848
KimJungChul <kjc5443@gmail.com>
2019-09-20 14:32:09 +0900

    update generate file_size code

--- aws/disk/gzip_compression/lambda_function.py
+++ aws/disk/gzip_compression/lambda_function.py
@@ -1,15 +1,23 @@
 from time import time
 import gzip
+import os
 
 
 def lambda_handler(event, context):
-    num_of_data = event['num_of_data']
-    data = "Hello World!" * num_of_data
+    file_size = event['file_size']
+    file_write_path = '/tmp/file'
 
     start = time()
-    with gzip.open('/tmp/result.gz', 'wb') as gz:
-        gz.writelines(data)
-    compress_latency = time() - start
+    with open(file_write_path, 'wb') as f:
+        f.write(os.urandom(file_size * 1024 * 1024))
+    disk_latency = time() - start
+
+    with open(file_write_path) as f:
+        start = time()
+        with gzip.open('/tmp/result.gz', 'wb') as gz:
+            gz.writelines(f)
+        compress_latency = time() - start
 
     print(compress_latency)
-    return compress_latency
+
+    return {'disk_write': disk_latency, "compress": compress_latency}

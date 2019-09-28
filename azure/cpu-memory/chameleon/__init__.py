b0a181470c91b3e8882b983f6ecf8dbd8d8bd683
KimJungChul <kjc5443@gmail.com>
2019-09-28 14:21:37 +0900

    adding chameleon

---
+++ azure/cpu-memory/chameleon/__init__.py
@@ -0,0 +1,37 @@
+import azure.functions as func
+import six
+from time import time
+from chameleon import PageTemplate
+
+
+BIGTABLE_ZPT = """\
+<table xmlns="http://www.w3.org/1999/xhtml"
+xmlns:tal="http://xml.zope.org/namespaces/tal">
+<tr tal:repeat="row python: options['table']">
+<td tal:repeat="c python: row.values()">
+<span tal:define="d python: c + 1"
+tal:attributes="class python: 'column-' + %s(d)"
+tal:content="python: d" />
+</td>
+</tr>
+</table>""" % six.text_type.__name__
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    num_of_rows = req.params.get('num_of_rows')
+    num_of_cols = req.params.get('num_of_cols')
+
+    start = time()
+    tmpl = PageTemplate(BIGTABLE_ZPT)
+
+    data = {}
+    for i in range(num_of_cols):
+        data[str(i)] = i
+
+    table = [data for x in range(num_of_rows)]
+    options = {'table': table}
+
+    data = tmpl.render(options=options)
+    latency = time() - start
+
+    return func.HttpResponse(str(latency))

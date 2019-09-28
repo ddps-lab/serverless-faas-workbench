f5a818961c2692a9eaa86b687473b9a4c534a3e7
KimJungChul <kjc5443@gmail.com>
2019-09-28 14:52:50 +0900

    adding pyaes

---
+++ google/cpu-memory/pyaes/main.py
@@ -0,0 +1,35 @@
+from time import time
+import random
+import string
+import pyaes
+
+
+def generate(length):
+    letters = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def function_handler(request):
+    request_json = request.get_json(silent=True)
+    length_of_message = request_json['length_of_message']
+    num_of_iterations = request_json['num_of_iterations']
+
+    message = generate(length_of_message)
+
+    # 128-bit key (16 bytes)
+    KEY = b'\xa1\xf6%\x8c\x87}_\xcd\x89dHE8\xbf\xc9,'
+
+    start = time()
+    for loops in range(num_of_iterations):
+        aes = pyaes.AESModeOfOperationCTR(KEY)
+        ciphertext = aes.encrypt(message)
+        print(ciphertext)
+
+        aes = pyaes.AESModeOfOperationCTR(KEY)
+        plaintext = aes.decrypt(ciphertext)
+        print(plaintext)
+        aes = None
+
+    latency = time() - start
+
+    return "latency : " + str(latency)

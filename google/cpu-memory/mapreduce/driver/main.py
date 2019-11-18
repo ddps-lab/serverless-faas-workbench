07d60e40f9373ee1dbc2daaa4da5db67ce72ff57
KimJungChul <kjc5443@gmail.com>
2019-11-18 17:23:14 +0900

    adding driver.py

---
+++ google/cpu-memory/mapreduce/driver/main.py
@@ -0,0 +1,71 @@
+from google.cloud import storage
+import gcsfs
+from functools import partial
+from multiprocessing.dummy import Pool as ThreadPool
+import time
+import json
+import requests
+
+
+total_map = 0
+total_network = 0
+
+
+def map_invoke_lambda(job_bucket, dataset_bucket, all_keys, batch_size, mapper_id):
+    keys = all_keys[mapper_id*batch_size: (mapper_id+1)*batch_size]
+    key = ""
+    for item in keys:
+        key += item +'/'
+    key = key[:-1]
+    
+    mapper_url = "https://us-central1-turing-gadget-230018.cloudfunctions.net/mr_mapper"
+    payload = {
+        'job_bucket': job_bucket,
+        'dataset_bucket': dataset_bucket,
+        'dataset_keys': key,
+        'mapper_id': mapper_id
+    }
+    
+    #headers = {'content-type' : 'application/json'}
+    
+    response = requests.post(mapper_url, json=payload)
+    print(response.url)
+    print(response.text)
+
+
+def function_handler(request):
+    request_json = request.get_json(silent=True)
+    job_bucket = request_json['job_bucket']
+    dataset_bucket = request_json['dataset_bucket']
+    n_mapper = int(request_json['n_mapper'])
+    
+    storage_client = storage.Client()
+    d_bucket = storage_client.get_bucket(dataset_bucket)
+    
+    # Fetch all the keys
+    dataset_blobs = d_bucket.list_blobs()
+    all_keys = []
+    for d_blob in dataset_blobs:
+    	all_keys.append(d_blob.name)
+
+    total_size = len(all_keys)
+    batch_size = 0
+    print("dataset file : " + str(all_keys))
+    print("key name : " + str(all_keys))
+    print("# of Mappers ", n_mapper)
+    
+    
+    if total_size % n_mapper == 0:
+        batch_size = int(total_size/n_mapper)
+    else:
+        batch_size = int(total_size/n_mapper) + 1
+    
+    for idx in range(n_mapper):
+        print("mapper-" + str(idx) + ":" + str(all_keys[idx*batch_size: (idx+1)*batch_size]))
+    
+    # Invoke Mapper
+    pool = ThreadPool(n_mapper)
+    invoke_lambda_partial = partial(map_invoke_lambda, job_bucket, dataset_bucket, all_keys, batch_size)
+    pool.map(invoke_lambda_partial, range(n_mapper))
+    pool.close()
+    pool.join()
